0abc5b16302301544d311c4f8ea30f67b9cf3747
Janelle Cueto <janellecueto@outlook.com>
2019-05-21 14:58:13 -0500

    updated

--- assets/php/Event.php
+++ assets/php/Event.php
@@ -24,7 +24,7 @@ function convertTo12($time24){
     //takes time string in 24 hour format and returns string as AM PM
     $hrs = substr($time24, 0, 2);
     $mins = substr($time24, 2, 2);
-    $seconds = substr($time24, 4);
+//    $seconds = substr($time24, 4);
     $ampm = "AM";
 
     if (intval($hrs) >= 12){
